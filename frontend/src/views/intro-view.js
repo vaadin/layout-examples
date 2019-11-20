7a45ba77d9c48cbdbc5c4e0fcb87456ccf51a81d
Kari Söderholm <haprog@vaadin.com>
2019-11-20 10:26:08 +0200

    fix: improve intro-view styles

--- frontend/src/views/intro-view.js
+++ frontend/src/views/intro-view.js
@@ -12,8 +12,21 @@ class IntroView extends PolymerElement {
 
     static get template() {
         return html`
-            <h1>Layout Examples for Vaadin 14</h1>
-            <p>Pick a layout from the menu.</p>
+            <style include="common-styles">
+                :host {
+                    display: block;
+                }
+                
+                .wrapper {
+                    margin: 0 auto;
+                    max-width: 960px;
+                    padding: 0 var(--lumo-space-m);
+                }
+            </style>
+            <main class="wrapper">
+                <h1>Layout Examples for Vaadin 14</h1>
+                <p>Pick a layout from the menu.</p>
+            </main>
         `;
     }
 
