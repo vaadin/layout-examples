0ad12183675c494a49c26877823712c82e2ee623
Manolo Carrasco <manolo@vaadin.com>
2023-03-13 13:32:21 +0100

    remove shared styles using legacy dom-modules and breaking the app

--- src/main/java/com/vaadin/demo/MainView.java
+++ src/main/java/com/vaadin/demo/MainView.java
@@ -9,7 +9,6 @@ import com.vaadin.flow.router.RouterLayout;
 /**
  * The main view is a top-level placeholder for other views.
  */
-@JsModule("./styles/shared-styles.js")
 @JsModule("./src/utils.js")
 @EnableGoogleAnalytics(value = "UA-658457-6", pageviewPrefix = "layout-examples/")
 public class MainView extends Div implements RouterLayout {
