533e8f0fff600e97d78714f0ba246e97bdbc3826
Manolo Carrasco <manolo@vaadin.com>
2023-03-13 13:36:34 +0100

    remove unsupported analytics addon in v24

--- src/main/java/com/vaadin/demo/MainView.java
+++ src/main/java/com/vaadin/demo/MainView.java
@@ -1,7 +1,5 @@
 package com.vaadin.demo;
 
-import org.vaadin.googleanalytics.tracking.EnableGoogleAnalytics;
-
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.RouterLayout;
@@ -10,7 +8,6 @@ import com.vaadin.flow.router.RouterLayout;
  * The main view is a top-level placeholder for other views.
  */
 @JsModule("./src/utils.js")
-@EnableGoogleAnalytics(value = "UA-658457-6", pageviewPrefix = "layout-examples/")
 public class MainView extends Div implements RouterLayout {
     public MainView() {
         getElement().getStyle().set("height", "100%");
